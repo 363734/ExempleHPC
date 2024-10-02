5fc5d204f60f9c36c1eb538e08952632a82b33bc
hverhaeghe <helene.verhaeghe27@gmail.com>
2024-10-02 15:50:43 +0200

    base project

--- pythonProject/hpc_script.sh
+++ pythonProject/hpc_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Submission script for Lemaitre4
 #SBATCH --job-name=test
+#SBATCH --output=log_test.txt
 #SBATCH --array=1-10
 #SBATCH --time=00:10:00 # hh:mm:ss
 #
@@ -11,6 +12,5 @@
 #SBATCH --mail-user=helene.verhaeghe27+cism@gmail.com
 #SBATCH --mail-type=ALL
 
-module purge
-module load LIST_THE_MODULES_YOU_NEED_HERE
-echo "Task ID: $SLURM_ARRAY_TASK_ID"
+source venv/bin/activate
+srun python hello.py $SLURM_ARRAY_TASK_ID
